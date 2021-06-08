e645c67b79876b522c7007bd122a232849b6569b
Brandon <copelandbrandon2@gmail.com>
2021-06-08 16:54:26 -0600

    will implement appointment sub-components

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+import InterviewerList from '../InterviewerList';
+import Button from '../Button';
+import { action } from '@storybook/addon-actions';
+import classNames from 'classnames';
+
+export default function Form(props) {
+  let nameVar;
+  const [usersName, setUsersName] = useState(props.name || "");
+  const [currentInterviewer, setInterviewer] = useState(props.interviewer || null);
+
+
+  const reset = function() {
+    setUsersName('');
+    setInterviewer(null);
+  };
+  const cancel = function() {
+    reset();
+    props.onCancel();
+  };
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={usersName}
+            onChange={(event) => setUsersName(event.target.value)}
+          />
+        </form>
+        <InterviewerList interviewers={props.interviewers} interviewer={currentInterviewer} setInterviewer={(event) => setInterviewer(event)} />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button onClick={() => cancel()} danger>Cancel</Button>
+          <Button onClick={props.onSave} confirm>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+};
